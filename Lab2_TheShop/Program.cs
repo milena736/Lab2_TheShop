644a446c4291a68b215bd4a64d2b544461f31271
milen <milen@JERMAINE>
2026-09-12 15:53:00 -0400

    Add project files.

---
+++ Lab2_TheShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2_TheShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Lab2_TheShop/Shop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2_TheShop
+{
+    internal class Shop
+    {
+    }
+}
